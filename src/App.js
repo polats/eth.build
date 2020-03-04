ec229c70116d18b97b7fc94f1eb18200b2708be7
Paul Gadi <paulgadi@gmail.com>
2020-03-04 13:50:28 +0800

    moved bottomMenu to a variable

--- src/App.js
+++ src/App.js
@@ -973,6 +973,102 @@ if(!showVideoLibrary && global.graph&&global.graph.canvas){
 }
 
 let extraMenus = ""
+let bottomMenu = ""
+
+bottomMenu = (
+  <div style={{zIndex:1,position:"fixed",height:barHeight,left:0,bottom:0,width:"100%"}}>
+    <div style={{borderRadius:"8px 8px 0px 0px",paddingLeft:6,margin:"auto",textAlign:"left",color:"#222222",height:barHeight,left:0,bottom:0,width:525,backgroundColor:"#DFDFDF"}}>
+      <div style={{cursor:"pointer",letterSpacing:-5,fontSize:32, fontFamily: "'Rubik Mono One', sans-serif"}}>
+
+        <span style={{margin:5,borderRight:"1px solid #cccccc",height:barHeight}} onClick={()=>{
+            liteGraphCanvas.switchLiveMode(true);
+            setLive(!live)
+            liteGraphCanvas.draw();
+          }}>
+          <Tooltip title={live?"Edit":"View"} style={{marginRight:10,cursor:"pointer"}}>
+            <Icon>
+              {live?"build":"visibility"}
+            </Icon>
+          </Tooltip>
+        </span>
+        <span style={{margin:5,borderRight:"1px solid #cccccc",height:barHeight}} onClick={()=>{
+            //console.log(liteGraph.status,playing)//cccccc.status==2
+            if(playing){
+              liteGraph.start()
+              setPlaying(false)
+            }else{
+              liteGraph.stop()
+              setPlaying(true)
+            }
+          }}>
+          <Tooltip title={playing?"Playing":"Fast Forwarding"} style={{marginRight:10,cursor:"pointer"}}>
+            <Icon>
+              {playing ? "play_circle_outline":"fast_forward"}
+            </Icon>
+          </Tooltip>
+        </span>
+
+        <span onClick={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
+          onTouchStart={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
+        >
+          <span style={{color:"#03a9f4"}}>eth</span>
+          <span style={{position:'relative',left:-5,bottom:15,color:"#f44336",marginBottom:25}}>.</span>
+          <span style={{position:'relative',left:-10,color:"#333"}}>build</span>
+        </span>
+
+        <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={()=>{
+            handleOpenSaveDialog()
+          }}>
+          <Tooltip title="Save" style={{marginLeft:10,cursor:"pointer"}}>
+            <Icon>
+              save
+            </Icon>
+          </Tooltip>
+        </span>
+        <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
+            // document.getElementById("loadjsonfile").click()
+            setOpenLoadDialog(true);
+          }}>
+          <Tooltip title="Load" style={{marginLeft:10,cursor:"pointer"}}>
+            <Icon>
+              open_in_browser
+            </Icon>
+          </Tooltip>
+        </span>
+        <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
+            setOpenAboutDialog(true)
+          }}>
+          <Tooltip title="About" style={{marginLeft:10,cursor:"pointer"}}>
+            <Icon>
+              info
+            </Icon>
+          </Tooltip>
+        </span>
+
+        <span style={{margin:5,paddingLeft:10,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
+            setReadQr(!readQr)
+          }}>
+          <Tooltip title="Scan" style={{marginLeft:10,cursor:"pointer"}}>
+            <svg style={{width:24,height:24,opacity:0.95}} viewBox="0 0 24 24">
+              <path fill="#000000" d="M4,4H10V10H4V4M20,4V10H14V4H20M14,15H16V13H14V11H16V13H18V11H20V13H18V15H20V18H18V20H16V18H13V20H11V16H14V15M16,15V18H18V15H16M4,20V14H10V20H4M6,6V8H8V6H6M16,6V8H18V6H16M6,16V18H8V16H6M4,11H6V13H4V11M9,11H13V15H11V13H9V11M11,6H13V10H11V6M2,2V6H0V2A2,2 0 0,1 2,0H6V2H2M22,0A2,2 0 0,1 24,2V6H22V2H18V0H22M2,18V22H6V24H2A2,2 0 0,1 0,22V18H2M22,22V18H24V22A2,2 0 0,1 22,24H18V22H22Z" />
+            </svg>
+          </Tooltip>
+        </span>
+
+        <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
+            setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true)
+          }}>
+          <Tooltip title="Learn More" style={{marginLeft:10,cursor:"pointer"}}>
+            <Icon>
+              swap_vert
+            </Icon>
+          </Tooltip>
+        </span>
+
+      </div>
+    </div>
+  </div>
+)
 
 if(!showVideoLibrary){
   extraMenus = (
@@ -1034,98 +1130,8 @@ return (
     <AboutDialog/>
     <SaveDialog liteGraph={liteGraph} setOpenSaveDialog={setOpenSaveDialog} openSaveDialog={openSaveDialog} dynamicWidth={dynamicWidth} screenshot={currentScreenShot} />
     <LoadDialog liteGraph={liteGraph} setOpenLoadDialog={setOpenLoadDialog} openLoadDialog={openLoadDialog} dynamicWidth={dynamicWidth} live={live} />
-    <div style={{zIndex:1,position:"fixed",height:barHeight,left:0,bottom:0,width:"100%"}}>
-      <div style={{borderRadius:"8px 8px 0px 0px",paddingLeft:6,margin:"auto",textAlign:"left",color:"#222222",height:barHeight,left:0,bottom:0,width:525,backgroundColor:"#DFDFDF"}}>
-        <div style={{cursor:"pointer",letterSpacing:-5,fontSize:32, fontFamily: "'Rubik Mono One', sans-serif"}}>
 
-          <span style={{margin:5,borderRight:"1px solid #cccccc",height:barHeight}} onClick={()=>{
-              liteGraphCanvas.switchLiveMode(true);
-              setLive(!live)
-              liteGraphCanvas.draw();
-            }}>
-            <Tooltip title={live?"Edit":"View"} style={{marginRight:10,cursor:"pointer"}}>
-              <Icon>
-                {live?"build":"visibility"}
-              </Icon>
-            </Tooltip>
-          </span>
-          <span style={{margin:5,borderRight:"1px solid #cccccc",height:barHeight}} onClick={()=>{
-              //console.log(liteGraph.status,playing)//cccccc.status==2
-              if(playing){
-                liteGraph.start()
-                setPlaying(false)
-              }else{
-                liteGraph.stop()
-                setPlaying(true)
-              }
-            }}>
-            <Tooltip title={playing?"Playing":"Fast Forwarding"} style={{marginRight:10,cursor:"pointer"}}>
-              <Icon>
-                {playing ? "play_circle_outline":"fast_forward"}
-              </Icon>
-            </Tooltip>
-          </span>
-
-          <span onClick={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
-            onTouchStart={()=>{setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true);}}
-          >
-            <span style={{color:"#03a9f4"}}>eth</span>
-            <span style={{position:'relative',left:-5,bottom:15,color:"#f44336",marginBottom:25}}>.</span>
-            <span style={{position:'relative',left:-10,color:"#333"}}>build</span>
-          </span>
-
-          <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={()=>{
-              handleOpenSaveDialog()
-            }}>
-            <Tooltip title="Save" style={{marginLeft:10,cursor:"pointer"}}>
-              <Icon>
-                save
-              </Icon>
-            </Tooltip>
-          </span>
-          <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
-              // document.getElementById("loadjsonfile").click()
-              setOpenLoadDialog(true);
-            }}>
-            <Tooltip title="Load" style={{marginLeft:10,cursor:"pointer"}}>
-              <Icon>
-                open_in_browser
-              </Icon>
-            </Tooltip>
-          </span>
-          <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
-              setOpenAboutDialog(true)
-            }}>
-            <Tooltip title="About" style={{marginLeft:10,cursor:"pointer"}}>
-              <Icon>
-                info
-              </Icon>
-            </Tooltip>
-          </span>
-
-          <span style={{margin:5,paddingLeft:10,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
-              setReadQr(!readQr)
-            }}>
-            <Tooltip title="Scan" style={{marginLeft:10,cursor:"pointer"}}>
-              <svg style={{width:24,height:24,opacity:0.95}} viewBox="0 0 24 24">
-                <path fill="#000000" d="M4,4H10V10H4V4M20,4V10H14V4H20M14,15H16V13H14V11H16V13H18V11H20V13H18V15H20V18H18V20H16V18H13V20H11V16H14V15M16,15V18H18V15H16M4,20V14H10V20H4M6,6V8H8V6H6M16,6V8H18V6H16M6,16V18H8V16H6M4,11H6V13H4V11M9,11H13V15H11V13H9V11M11,6H13V10H11V6M2,2V6H0V2A2,2 0 0,1 2,0H6V2H2M22,0A2,2 0 0,1 24,2V6H22V2H18V0H22M2,18V22H6V24H2A2,2 0 0,1 0,22V18H2M22,22V18H24V22A2,2 0 0,1 22,24H18V22H22Z" />
-              </svg>
-            </Tooltip>
-          </span>
-
-          <span style={{margin:5,borderLeft:"1px solid #cccccc",height:barHeight}} onClick={async ()=>{
-              setShowVideoLibrary(true);global.showLibrary=true;localStorage.setItem("eth.build.showLibrary",true)
-            }}>
-            <Tooltip title="Learn More" style={{marginLeft:10,cursor:"pointer"}}>
-              <Icon>
-                swap_vert
-              </Icon>
-            </Tooltip>
-          </span>
-
-        </div>
-      </div>
-    </div>
+    {bottomMenu}
 
 
 
